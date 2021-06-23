d90e2c0afbf4f3ebb09a51f8f5706c32b870f515
Perry Brandiezs <pbrandiezs@gmail.com>
2021-06-23 10:43:33 -0500

    feat: Lesson 254 Sending a POST Request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,12 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http.post(
+      'https://ng-complete-guide-d0c0b-default-rtdb.firebaseio.com/posts.json',
+      postData
+    ).subscribe(responseData => {
+      console.log(responseData);
+    });
   }
 
   onFetchPosts() {
